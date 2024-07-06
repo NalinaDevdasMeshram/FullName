ee5c7d7fca0fe06b20fe3bdda8443a0ead015af4
nalina <naleenameshram85@gmail.com>
2024-07-06 20:00:13 +0530

    fullname

--- DisplayFullName/src/Component/FullName.jsx
+++ DisplayFullName/src/Component/FullName.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 
  const FullName = ()=>{
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname]  = useState('');
+    const [fullname, setFullname] = useState('')
+   const handleSubmit =(e)=>{
+       e.preventDefault();
+        setFullname (`${firstname} ${lastname}`);
+    }
     return (
         <div>
-            welcome to FullName
+            <form onSubmit={handleSubmit}>
+            <h1>Full Name Display</h1>
+            <div>
+            First Name:<input type="text" name="firstname" id="firstname"  value={firstname} 
+            onChange={(e)=>setFirstname(e.target.value)}required/>
+           </div>
+           <div>
+            Last Name:<input type="text" name="lastname" id="lastname"  value={lastname}  onChange={(e)=>setLastname(e.target.value)}required/>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+        {fullname && <p>Full Name:{fullname}</p>}
         </div>
     )
  }
