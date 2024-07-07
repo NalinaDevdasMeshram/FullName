dd7a0c90d6fe9bc3d704a3899381fc97a3c68b0a
nalina <naleenameshram85@gmail.com>
2024-07-07 12:47:56 +0530

    changed

--- DisplayFullName/src/Component/FullName.jsx
+++ DisplayFullName/src/Component/FullName.jsx
@@ -3,30 +3,32 @@ const FullName = ()=>{
     const [formData, setFormData] = useState({firstname:'', lastname:''});
     const [fullname, setFullname] = useState('')
     const handleChange =(e)=>{
-          const {name, value} =e.target;
+          const {name, value} = e.target;
           setFormData({...formData,
             [name]:value,
           });
     }
    const handleSubmit =(e)=>{
       setFullname (`${formData.firstname} ${formData.lastname}`);
-      e.preventDefault();
+    //   e.preventDefault();
         setFormData({firstname:'', lastname:''})
+        e.preventDefault();
     }
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit = {handleSubmit}>
             <h1>Full Name Display</h1>
             <div>
-            First Name:<input type="text" name="firstname" id="firstname"  value={formData.firstname} 
+            First Name: <input type="text" name="firstname" id="firstname"  value={formData.firstname} 
             onChange={handleChange}required/>
            </div>
            <div>
-            Last Name:<input type="text" name="lastname" id="lastname"  value={formData.lastname}  onChange={handleChange}required/>
+            Last Name: <input type="text" name="lastname"  id="lastname"  value={formData.lastname}  
+            onChange={handleChange}required/>
             </div>
-            <button type="submit">Submit</button>
+            <button type="submit"> Submit </button>
         </form>
-        { fullname && <p>Full Name: {fullname}</p>}
+        {fullname && <p>Full Name: {fullname}</p>}
         </div>
     )
  }
