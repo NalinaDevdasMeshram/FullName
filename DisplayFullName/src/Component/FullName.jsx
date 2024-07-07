55a8e46c51e5b9a6a58842119aab77744151e914
nalina <naleenameshram85@gmail.com>
2024-07-07 12:24:58 +0530

    changes

--- DisplayFullName/src/Component/FullName.jsx
+++ DisplayFullName/src/Component/FullName.jsx
@@ -1,29 +1,32 @@
 import { useState } from "react";
 const FullName = ()=>{
-    const [firstname, setFirstname] = useState('');
-    const [lastname, setLastname]  = useState('');
+    const [formData, setFormData] = useState({firstname:'', lastname:''});
     const [fullname, setFullname] = useState('')
+    const handleChange =(e)=>{
+          const {name, value} =e.target;
+          setFormData({...formData,
+            [name]:value,
+          });
+    }
    const handleSubmit =(e)=>{
-      setFullname (`${firstname} ${lastname}`);
+      setFullname (`${formData.firstname} ${formData.lastname}`);
       e.preventDefault();
-        setFirstname(''); // clearing the firstname
-        setLastname('');// clearing the lastname
-        //  e.preventDefault();
+        setFormData({firstname:'', lastname:''})
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
             <h1>Full Name Display</h1>
             <div>
-            First Name:<input type="text" name="firstname" id="firstname"  value={firstname} 
-            onChange={(e)=>setFirstname(e.target.value)}required/>
+            First Name:<input type="text" name="firstname" id="firstname"  value={formData.firstname} 
+            onChange={handleChange}required/>
            </div>
            <div>
-            Last Name:<input type="text" name="lastname" id="lastname"  value={lastname}  onChange={(e)=>setLastname(e.target.value)}required/>
+            Last Name:<input type="text" name="lastname" id="lastname"  value={formData.lastname}  onChange={handleChange}required/>
             </div>
             <button type="submit">Submit</button>
         </form>
-        {fullname && <p>Full Name: {fullname}</p>}
+        { fullname && <p>Full Name: {fullname}</p>}
         </div>
     )
  }
